bbb7e8674076692b5e1ba721937f48d651605ce1
luizclaudioperdigao <luiz.perdigao@stellantis.com>
2025-07-10 21:16:55 -0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,9 @@
+import time
+
+print("✅ Bot Python iniciado com sucesso no Fly.io!")
+
+for i in range(3):
+    print(f"⌛ Esperando... ({i+1}/3)")
+    time.sleep(5)
+
+print("✅ Execução finalizada com sucesso.")
