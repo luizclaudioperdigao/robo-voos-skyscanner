025549effe359253d1f897fb98a422458205bf62
luizclaudioperdigao <luiz.perdigao@stellantis.com>
2025-07-10 21:39:45 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,8 @@ import time
 
 print("✅ Bot Python iniciado com sucesso no Fly.io!")
 
-for i in range(3):
-    print(f"⌛ Esperando... ({i+1}/3)")
-    time.sleep(5)
-
-print("✅ Execução finalizada com sucesso.")
- 
+contador = 0
+while True:
+    contador += 1
+    print(f"⌛ Loop ativo: {contador}")
+    time.sleep(10)
